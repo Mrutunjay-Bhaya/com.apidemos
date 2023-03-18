44e00c7b8c06306eb56600847def71080fe4c50e
Mrutunjay-Bhaya <mrutunjaybhayasibu@gmail.com>
2023-03-18 18:59:53 +0530

    Script for APIdemo application

--- src/test/java/appium/appiumtest.java
+++ src/test/java/appium/appiumtest.java
@@ -1,62 +1,133 @@
 package appium;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.sun.accessibility.internal.resources.accessibility;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.PerformsTouchActions;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.screenrecording.BaseStartScreenRecordingOptions;
+import io.appium.java_client.touch.TapOptions;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.ElementOption;
+import io.appium.java_client.touch.offset.PointOption;
 
 public class appiumtest {
 
-	private AndroidDriver driver;
+	private AndroidDriver<MobileElement> driver;
 
 	@BeforeTest
 	public void setUp() throws MalformedURLException {
 		DesiredCapabilities dc = new DesiredCapabilities();
-		dc.setCapability("platformName", "Android");
-		dc.setCapability("appium:platformVersion", "11");
-		dc.setCapability("appium:udid", "y5dudepvnzpbd6jj");
-		dc.setCapability("appium:ensureWebviewsHavePages", true);
-		dc.setCapability("appium:nativeWebScreenshot", true);
-		dc.setCapability("appium:newCommandTimeout", 3600);
-		dc.setCapability("appium:connectHardwareKeyboard", true);
-	   	dc.setCapability("appPackage", "com.miui.calculator");
-		dc.setCapability("appActivity", "com.miui.calculator.cal.CalculatorTabActivity");
+		dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");// XCUITest for iOS device
+		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");// platform name should be iOS or Android
+		dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
+		dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Mi");// device name can be anything
+		dc.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 3600);
+		dc.setCapability("autoGrantPermissions", true);
+		/*   adb shell dumpsys window | find "mCurrentFocus"
+		type this command in cmd to know you apppackage and appactivity on current device
+		*/
+		dc.setCapability("appPackage", "io.appium.android.apis");
+		dc.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
 		URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 
-		driver = new AndroidDriver(remoteUrl, dc);
+		driver = new AndroidDriver<MobileElement>(remoteUrl, dc);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
 
 	@Test
 	public void sampleTest() throws Throwable {
-		/*
-		 * driver.findElement(By.xpath(
-		 * "//android.widget.ImageView[@content-desc=\"Messaging\"]")).click();
-		 * driver.findElement(By.xpath(
-		 * "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/" +
-		 * "android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]"
-		 * +
-		 * "/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup"
-		 * + "/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]")).
-		 * click(); Thread.sleep(2000); driver.findElement(By.xpath(
-		 * "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.RelativeLayout/android.widget.EditText"
-		 * )) .sendKeys("I LOVE YOU"); Thread.sleep(2000);
-		 * 
-		 * 
-		 * driver.findElement(By.id("com.android.mms:id/send_button")).click();
-		 */
+//		driver.unlockDevice();
+//		driver.toggleData();
+//	
+//		//driver.toggleWifi();
+//		  Thread.sleep(2000);
+		// driver.hideKeyboard();
 
+//		  File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//	        
+//			FileUtils.copyFile(scrFile, new File("./screen/image.png"));
+		// Get the size of the device screen
+		Thread.sleep(2000);
+		driver.findElement(By.id("com.android.permissioncontroller:id/continue_button")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("android:id/button1")).click();
+		// we can use this xpath or find the element from list of element
+
+		// driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Views\"]")).click();
+		driver.findElementsById("android:id/text1").get(10).click();
+		// WebElement el=driver.findElement(By.id("android:id/text1"));
+		WebElement list = driver
+				.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView("
+						+ "new UiSelector().description(\"Splitting Touches across Views\"));"));
+		list.click();
+		WebElement list1 = driver.findElement(MobileBy.AndroidUIAutomator(
+				"new UiScrollable(new UiSelector()).scrollIntoView(" + "new UiSelector().text(\"Brin\"));"));
+		list1.click();
+		driver.navigate().back();
+		driver.navigate().back();
+
+		driver.findElementsById("android:id/text1").get(10).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Drag and Drop\"]")).click();
+		WebElement source = driver.findElement(By.id("io.appium.android.apis:id/drag_dot_3"));
+		WebElement target = driver.findElement(By.id("io.appium.android.apis:id/drag_dot_2"));
+		TouchAction ac = new TouchAction(driver);
+
+		// drag and drop
+		ac.longPress(ElementOption.element(source)).moveTo(ElementOption.element(target)).release().perform();
+		driver.navigate().back();
+		driver.navigate().back();
+		driver.findElementsById("android:id/text1").get(10).click();
+		Dimension size = driver.manage().window().getSize();
+
+		// Set the start and end coordinates for the scrolling action
+		int startX = size.width / 2;
+		int startY = (int) (size.height * 0.8);
+		int endY = (int) (size.height * 0.1);
+
+		// Create a TouchAction instance
+
+		// Thread.sleep(5000);
+		// Perform the scrolling action
+		ac.press(PointOption.point(startX, startY)).waitAction(WaitOptions.waitOptions(Duration.ofSeconds(3)))
+				.moveTo(PointOption.point(startX, endY)).release().perform();
+		driver.findElementsById("android:id/text1").get(8).click();
+		driver.findElementsById("android:id/text1").get(10).click();
+		WebElement al = driver.findElementsById("android:id/text1").get(0);
+		//al.click();
+		//Thread.sleep(1000);
+		//al.click();
+		driver.findElementByAccessibilityId("");
+	
+		ac.tap(TapOptions.tapOptions().withElement(ElementOption.element(al)))
+        .tap(TapOptions.tapOptions().withElement(ElementOption.element(al)))
+        .perform();
 	}
 
 	@AfterTest
 	public void tearDown() {
-		//driver.quit();
+		// driver.quit();
 	}
 }
